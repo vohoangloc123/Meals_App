17a0e07bf2e56b6df847eac2499932431ea4e7ee
vohoangloc123 <you@example.com>
2024-09-10 15:44:18 +0700

    Improved the MealDetails Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/screens/categories.dart';
-import 'package:meals_app/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
--- lib/widgets/meal_details.dart
+++ lib/widgets/meal_details.dart
@@ -10,8 +10,46 @@ class MealDetails extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(meal.imageUrl,
-          height: 300, width: double.infinity, fit: BoxFit.cover),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(meal.imageUrl,
+                height: 300, width: double.infinity, fit: BoxFit.cover),
+            const SizedBox(height: 10),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            for (final ingredient in meal.ingredients)
+              Text(ingredient,
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      )),
+            const SizedBox(height: 10),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(height: 10),
+            for (final step in meal.steps)
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: Text(step,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        )),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
