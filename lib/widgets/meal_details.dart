cfea924162f879656a5b26821f8ee6e94f908afa
vohoangloc123 <you@example.com>
2024-09-21 11:29:09 +0700

    Add Multi-Screen Transitions

--- lib/widgets/meal_details.dart
+++ lib/widgets/meal_details.dart
@@ -49,8 +49,11 @@ class MealDetails extends ConsumerWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(meal.imageUrl,
-                height: 300, width: double.infinity, fit: BoxFit.cover),
+            Hero(
+              tag: meal.id,
+              child: Image.network(meal.imageUrl,
+                  height: 300, width: double.infinity, fit: BoxFit.cover),
+            ),
             const SizedBox(height: 10),
             Text(
               'Ingredients',
